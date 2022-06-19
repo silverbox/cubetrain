fc81cff25c8c78ce1222949c73fc8e0282182f72
Eiji Tanaka <tanaka.eiji@nifty.com>
2022-06-19 20:16:38 +0900

    fix build script

--- wasm/build.sh
+++ wasm/build.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 VUE_OUTPUT_PATH='../vue/cubetrain/src/wasm'
+VUE_WASM_OUTPUT_PATH='../vue/cubetrain/public/wasm'
 NGINX_OUTPUT_PATH='../nginx/wasm'
 
 cargo make build_release && \
@@ -11,6 +12,7 @@ echo 'if (typeof global.TextEncoder === "undefined") { global.TextEncoder = requ
 echo 'if (typeof global.TextDecoder === "undefined") { global.TextDecoder = require("util").TextDecoder }' >> ${VUE_OUTPUT_PATH}/package.js && \
 echo 'if (typeof global.fetch === "undefined") { const fetchPolifill = require("whatwg-fetch"); global.fetch = fetchPolifill.fetch;}' >> ${VUE_OUTPUT_PATH}/package.js && \
 cat pkg/package.js >> ${VUE_OUTPUT_PATH}/package.js && \
-cp pkg/package_bg.wasm ${VUE_OUTPUT_PATH}/package_bg.wasm && \
+sed -i -e "s/import\.meta\.url/import_meta_url/g" ${VUE_OUTPUT_PATH}/package.js && \
+cp pkg/package_bg.wasm ${VUE_WASM_OUTPUT_PATH}/package_bg.wasm && \
 cp pkg/package.js ${NGINX_OUTPUT_PATH}/package.js && \
 cp pkg/package_bg.wasm ${NGINX_OUTPUT_PATH}/package_bg.wasm
