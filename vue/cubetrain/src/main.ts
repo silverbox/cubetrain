f65ed2df96297a08bf155749144dfce945968ea0
Eiji Tanaka <tanaka.eiji@nifty.com>
2022-07-17 11:47:50 +0900

    add module

--- vue/cubetrain/src/main.ts
+++ vue/cubetrain/src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { Vue3Mq } from "vue3-mq";
 
 loadFonts()
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App);
+app.use(vuetify);
+app.use(Vue3Mq, {
+  preset: "vuetify"
+});
+app.mount('#app')
